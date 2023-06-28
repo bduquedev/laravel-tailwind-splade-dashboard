0397ec377a1e8f42da4ae174b7a816fb7214f9a1
bduquedev <bduque.dev@gmail.com>
2023-06-28 15:11:57 +0600

    wip: theme setup

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,13 @@
+@include('includes.navbar')
+
+<x-splade-data store="sidebar" default="{ opened: false }" />
+
+<div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
+    @include('includes.sidebar')
+    <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
+        <main>
+            @yield("main")
+        </main>
+        @include('includes.footer')
+    </div>
+</div>
--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        @spladeHead
-        @vite('resources/js/app.js')
-    </head>
-    <body class="font-sans antialiased">
-        @splade
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @spladeHead
+    @vite('resources/js/app.js')
+
+    <script>
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    </script>
+</head>
+
+<body class="font-sans antialiased bg-gray-50 dark:bg-gray-800">
+    @splade
+</body>
+
 </html>
