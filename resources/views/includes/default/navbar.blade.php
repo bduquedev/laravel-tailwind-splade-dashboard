632794dcaaf0d7e3a140951f06cbc8cc1daab829
bduquedev <bduque.dev@gmail.com>
2023-06-30 00:48:44 +0600

    refactor: auth conditions

--- resources/views/includes/default/navbar.blade.php
+++ resources/views/includes/default/navbar.blade.php
@@ -1,5 +1,5 @@
 <x-splade-toggle>
-    <nav class="fixed z-50 w-full bg-white border-b border-gray-200 sm:py-2 dark:bg-gray-800 dark:border-gray-700">
+    <nav class="fixed z-30 w-full bg-white border-b border-gray-200 sm:py-2 dark:bg-gray-800 dark:border-gray-700">
         <div class="container py-3 mx-auto">
             <div class="flex items-center justify-between">
                 <div class="flex items-center justify-start">
@@ -28,10 +28,16 @@
                         <i class="bi bi-sun"></i>
                     </button>
                     <!-- !!End Change theme!! -->
-
+                    
+                    @if(auth()->check())
+                    <x-splade-link :href="route('default:dashboard')" class="hidden sm:inline-flex text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                        <i class="bi bi-person text-lg mr-1"></i> Dashboard
+                    </x-splade-link>
+                    @else
                     <x-splade-link :href="route('default:auth.login')" class="hidden sm:inline-flex text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                         <i class="bi bi-box-arrow-in-left text-lg"></i> Login/Register
                     </x-splade-link>
+                    @endif
 
                     <button @click="toggle" class="inline-flex items-center justify-center p-2  text-gray-400 rounded-lg sm:hidden mr-2 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-primary-300 dark:hover:bg-gray-700 dark:hover:text-white">
                         <i class="bi bi-list"></i>
@@ -55,9 +61,15 @@
                 </li>
 
                 <li>
+                    @if(auth()->check())
+                    <x-splade-link :href="route('default:dashboard')" class="block py-2 pl-3 pr-4 text-base font-normal text-gray-600 dark:text-gray-400">
+                        Go to Dashboard
+                    </x-splade-link>
+                    @else
                     <x-splade-link :href="route('default:auth.login')" class="inline-flex items-center w-full px-3 py-2 text-base font-normal {{ request()->routeIs('default:auth.*') ? 'text-gray-900 dark:text-white bg-gray-100 dark:bg-gray-700 ':'text-gray-600 dark:text-gray-400' }}">
                         <i class="bi bi-box-arrow-in-left text-lg"></i> Login/Register
                     </x-splade-link>
+                    @endif
                 </li>
             </ul>
         </x-splade-transition>
