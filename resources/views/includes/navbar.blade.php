91276efa0b08a1101a1cb748d04303082b68fd3b
bduquedev <bduque.dev@gmail.com>
2023-06-29 11:34:49 +0600

    refactor: remove unused button

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -32,10 +32,6 @@
                 @endif
                 <!-- !!Remove if you are in production!!  -->
 
-                <button @click.prevent="sidebar.opened = !sidebar.opened" type="button" class="p-2 text-gray-500 rounded-lg lg:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                    <i class="bi bi-search text-gray-500 text-lg"></i>
-                </button>
-
                 <button type="button" class="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">
                     <i class="bi bi-bell-fill text-xl"></i>
                 </button>
