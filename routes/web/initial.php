8be6589617642b23a526a0347cd6f378832c74b9
bduquedev <bduque.dev@gmail.com>
2023-06-29 17:39:04 +0600

    removed home route

--- routes/web/initial.php
+++ routes/web/initial.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\Initial\ShowHomePageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", ShowHomePageController::class)->middleware("auth")->name("home");
-
 Route::get("/dashboard", ShowHomePageController::class)->middleware("auth")->name("dashboard");
