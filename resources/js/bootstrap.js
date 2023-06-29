f9f4ee65014f9b0ee132f1c2768320c8c1139432
bduquedev <bduque.dev@gmail.com>
2023-06-29 17:37:25 +0600

    refactor: removed unnecessary

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,21 +1,9 @@
 import axios from 'axios';
+import { applyTheme } from './global-helpers';
 
 // API related configurations 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // tailwind theming
-window.applyTheme = (change = false) => {
-
-    if (change !== false) {
-        localStorage.theme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
-    }
-
-    if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark');
-    } else {
-        document.documentElement.classList.remove('dark')
-    }
-}
-
-window.addEventListener("load", () => window.applyTheme());
+window.addEventListener("load", () => applyTheme());
