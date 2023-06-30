b1d475a49ca1297f4d786f0ee9c867fdec25fc5f
bduquedev <bduque.dev@gmail.com>
2023-06-30 10:33:10 +0600

    refactor: on first click apply inverse current device theme

--- resources/js/global-helpers.js
+++ resources/js/global-helpers.js
@@ -10,7 +10,20 @@ export function applyTheme() {
 
 // change theme and apply theme
 export function changeAndApplyTheme(theme = null) {
-    localStorage.theme = theme !== null ? theme : localStorage.getItem('theme') === 'dark' ? "light" : "dark";
-    applyTheme();
-    return;
+    // get the current theme from local storage
+    const currentTheme = localStorage.getItem('theme');
+    // if current theme is in light mode
+    if (currentTheme === 'light') {
+        localStorage.theme = 'dark';
+    }
+    // if current theme is in dark mode
+    else if (currentTheme === 'dark') {
+        localStorage.theme = 'light';
+    }
+    // otherwise inject device current theme 
+    else {
+        localStorage.theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'light' : 'dark';
+    }
+    // apply the theme into the app
+    return applyTheme();
 }
