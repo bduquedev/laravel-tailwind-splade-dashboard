f7b66bf367c154cba4c84fbeac60a720b6f68ffc
bduquedev <bduque.dev@gmail.com>
2023-06-30 10:38:45 +0600

    refactor: padding style fixing

--- resources/views/layouts/authentication.blade.php
+++ resources/views/layouts/authentication.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.default')
 
 @section('body')
-<div class="flex flex-col items-center justify-center pb-11 md:pb-10 mx-auto md:pb-32 lg:px-0 mx-auto dark:bg-gray-900">
+<div class="flex flex-col items-center justify-center pb-9 md:pb-9 mx-auto md:pb-32 lg:px-0 mx-auto dark:bg-gray-900">
     <!-- Card -->
-    <div class="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow dark:bg-gray-800">
+    <div class="w-full max-w-xl p-6 sm:p-8 bg-white rounded-lg shadow dark:bg-gray-800">
         <h2 class="text-2xl font-bold text-gray-900 dark:text-white">
             @yield('form_title')
         </h2>
